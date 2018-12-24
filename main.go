1ac638013c312d4ee5a55a8fbb0f0a47edc12a42
Grace Wang <gracewang92@gmail.com>
2018-12-24 02:10:29 -0500

    use graphql client instead of go-github (#3)

--- main.go
+++ main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gracew/repo-health/repohealth"
 	"github.com/julienschmidt/httprouter"
+	"github.com/machinebox/graphql"
 
-	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
 
 func main() {
 	router := httprouter.New()
-	router.GET("/:org/:repo/score", scoreRepository)
+	router.GET("/:org/:name/score", scoreRepository)
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		panic(err)
 	}
@@ -37,10 +39,17 @@ func scoreRepository(w http.ResponseWriter, r *http.Request, params httprouter.P
 		AccessToken: os.Getenv("ACCESS_TOKEN"),
 	}
 	oauthClient := oauth2.NewClient(oauth2.NoContext, tokenSource)
-	client := github.NewClient(oauthClient)
+	client := graphql.NewClient("https://api.github.com/graphql", graphql.WithHTTPClient(oauthClient))
 
-	issueScore := repohealth.GetIssueScore(client, params.ByName("org"), params.ByName("repo"))
-	prScore := repohealth.GetPRScore(client, params.ByName("org"), params.ByName("repo"))
+	org := params.ByName("org")
+	name := params.ByName("name")
+	queryValues := r.URL.Query()
+	numWeeks, err := strconv.Atoi(queryValues.Get("weeks"))
+	if err != nil {
+		log.Panic(err)
+	}
+	issueScore := repohealth.GetIssueScore(client, org, name, numWeeks)
+	prScore := repohealth.GetPRScore(client, org, name, numWeeks)
 	repoScore := repohealth.RepositoryScore{Issues: issueScore, PRs: prScore}
 	json.NewEncoder(w).Encode(repoScore)
 }
